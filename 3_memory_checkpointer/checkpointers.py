1eaa1c8d6bcaa72ee89e5a1175525f7661fab7b2
Rohit <rkvcone1323@gmail.com>
2026-03-19 20:53:06 +0530

    modify and make checkpointer working

--- 3_memory_checkpointer/checkpointers.py
+++ 3_memory_checkpointer/checkpointers.py
@@ -12,10 +12,16 @@ def generate_product_id() -> int:
     "Generate a random ID for the product"
     return random.randint(1, 100)
 
+def add_product_to_card(product_id: int) -> str:
+    "Simulate adding a product to a shopping cart"
+    with open("cart.md", "a") as f:
+        f.write(f"Added ITEM-{product_id} to your cart.\n")
+    return f"Added ITEM-{product_id} to your cart."
+
 def llm_with_tool(state: MessagesState) -> str:
     "Simulate an LLM that calls a tool to generate a product ID"
     llm = get_model()
-    llm_with_tool = llm.bind_tools([generate_product_id])
+    llm_with_tool = llm.bind_tools([generate_product_id, add_product_to_card])
     return {"messages": [llm_with_tool.invoke(state["messages"])]}
 
 # define the graph
@@ -25,7 +31,7 @@ memory = InMemorySaver()
 
 # add nodes
 builder.add_node("llm_with_tool", llm_with_tool)
-builder.add_node("tools", ToolNode([generate_product_id]))
+builder.add_node("tools", ToolNode([generate_product_id, add_product_to_card]))
 
 # add edges
 builder.add_edge(START, "llm_with_tool")
@@ -44,13 +50,17 @@ if __name__ == "__main__":
     # print_langchain_messages(result["messages"])
 
     # with tool calls
-    result = graph.invoke({"messages": ["Generate a product ID for me."]}, config=configuration)
+    result = graph.invoke({"messages": ["Generate a product ID for me."]}, configuration)
     print_langchain_messages(result["messages"])
+
+
+    print("-" * 10 + " Testing Config " + "-" * 10)
 
 
     # now here we are not passing the previous messages yet it will be able to access the previous
     # messages and config from the memory checkpointer and give us the correct response.
-    result2 = graph.invoke({"messages": ["Add ITEM- in front of product id and give back to me"]}, config=configuration)
+    result2 = graph.invoke({"messages": ["Add the generated product id to the cart."]}, configuration)
+    print_langchain_messages(result2["messages"])
 
 
 
